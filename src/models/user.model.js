d542f9e7834c38140b4f3def284643ee2414a769
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-15 21:46:56 +0200

    Created User Schema, virtual and hash

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,60 @@
-// TODO add user model
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 25,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  try {
+    if (user.isModified("password" || user.isNew)) {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
