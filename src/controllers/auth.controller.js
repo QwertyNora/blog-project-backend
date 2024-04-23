782f7b9d35a3f2e7a36ec476f6ad0f1153d1534b
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-23 19:04:18 +0200

    added fullName to user model

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,39 @@
-// TODO setup auth controllers
 const User = require("../models/user.model");
 
 async function registerUser(req, res) {
   const _user = req.body;
   try {
     const user = await User.create(_user);
-    res.status(201).send(user);
+    const userObject = user.toObject();
+    userObject.fullName = user.fullName;
+    res.status(201).send({
+      username: userObject.username,
+      fullName: userObject.fullName,
+      email: userObject.email,
+      fullname: userObject.fullName,
+    });
   } catch (error) {
-    throw new Error("Error occured: " + error.message + error);
+    res.status(400).send({ message: "Error occurred: " + error.message });
   }
 }
 
 module.exports = {
   registerUser,
 };
+
+// // TODO setup auth controllers
+// const User = require("../models/user.model");
+
+// async function registerUser(req, res) {
+//   const _user = req.body;
+//   try {
+//     const user = await User.create(_user);
+//     res.status(201).send(user);
+//   } catch (error) {
+//     throw new Error("Error occured: " + error.message + error);
+//   }
+// }
+
+// module.exports = {
+//   registerUser,
+// };
