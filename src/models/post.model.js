9b907576284064c3821335a0475fc4a23136fcaa
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-24 15:07:45 +0200

    post model

---
+++ src/models/post.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
